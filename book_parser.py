e4c8a420f5df0e66314602c9334e99e3e2171df1
statik2002 <statik2002@mail.ru>
2022-09-06 17:44:37 +0800

    parse books genres2

--- book_parser.py
+++ book_parser.py
@@ -63,7 +63,12 @@ for i in range(1, 10):
         #if book_image_url:
         #    download_img(urljoin(url, book_image_url["src"]))
 
-        book_comments = soup.find_all('div', class_='texts')
+        #book_comments = soup.find_all('div', class_='texts')
+        #print(book_name)
+        #for comment in book_comments:
+        #    print(f'-- {comment.find("span", class_="black").text}')
+
         print(book_name)
-        for comment in book_comments:
-            print(f'-- {comment.find("span", class_="black").text}')
+        book_genres = soup.find('span', class_='d_book').find_all('a')
+        for genre in book_genres:
+            print(genre.text, end=',')
