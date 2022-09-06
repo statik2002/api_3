242c42fb5f04b83c42ae678d4fc5fdc9f7ca7b43
statik2002 <statik2002@mail.ru>
2022-09-06 12:07:43 +0800

    download book covers

--- book_parser.py
+++ book_parser.py
@@ -1,5 +1,6 @@
 import os.path
 import requests
+from urllib.parse import urljoin, urlsplit, unquote
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 
@@ -7,6 +8,19 @@ from bs4 import BeautifulSoup
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
+
+
+def download_img(url, folder='images/'):
+
+    response = requests.get(url)
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    filepath = os.path.join(folder, urlsplit(url)[2].split('/')[2])
+
+    with open(unquote(filepath), 'wb') as file:
+        file.write(response.content)
 
 
 def download_txt(url, filename, folder='books/'):
@@ -42,3 +56,7 @@ for i in range(1, 10):
     book_url = soup.find('a', text='скачать txt')
     if book_url:
         download_txt(f'{url[:-2]}{book_url["href"]}', book_name)
+        book_image_url = soup.find('div', class_='bookimage').find('a').find('img')
+        if book_image_url:
+            download_img(urljoin(url, book_image_url["src"]))
+
