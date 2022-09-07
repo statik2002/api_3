32920d99fc2e9b2027112f756336ad3ae04be9e3
statik2002 <statik2002@mail.ru>
2022-09-07 18:50:37 +0800

    Урок 12 API_3
    Сделано:
    1. Создал функцию parse_book_page()
    2. Эта функция создает словарь, который содержит название книги, автора, ссылку на txt, ссылку на картинку, список комментов, список жанров
    3. выводит список
    
    Доп. к заданию, добавил функцию по очистке строки от ненужных символов.

--- book_parser.py
+++ book_parser.py
@@ -3,6 +3,7 @@ import requests
 from urllib.parse import urljoin, urlsplit, unquote
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 
 def check_for_redirect(response):
@@ -46,6 +47,49 @@ def download_txt(url, filename, folder='books/'):
 def download_comments(url):
     pass
 
+
+def clear_string(string):
+    return string.replace('\xa0', '')
+
+
+def parse_book_page(page_soup):
+
+    try:
+        check_for_redirect(response)
+        book = dict()
+
+        book['name'] = clear_string(page_soup.find('h1').text.split('::')[0]).strip()
+        book['author'] = clear_string(page_soup.find('h1').text.split('::')[1]).strip()
+        book_url = page_soup.find('a', text='скачать txt')
+
+        if book_url:
+            #download_txt(f'{url[:-2]}{book_url["href"]}
+            book['txt_url'] = f'{url[:-2]}{book_url["href"]}'
+            book_image_url = page_soup.find('div', class_='bookimage').find('a').find('img')
+            if book_image_url:
+                # download_img(urljoin(url, book_image_url["src"]))
+                book['image'] = urljoin(url, book_image_url["src"])
+
+            book_comments = page_soup.find_all('div', class_='texts')
+            #print(book_name)
+            comments = []
+            for comment in book_comments:
+                #print(f'-- {comment.find("span", class_="black").text}')
+                comments.append(comment.find("span", class_="black").text)
+            book['comments'] = comments
+
+            genres = []
+            book_genres = list(page_soup.find('span', class_='d_book').find_all('a'))
+            for genre in book_genres:
+                genres.append(genre.text)
+
+            book['genres'] = genres
+
+            pprint(book)
+    except:
+            pass
+
+
 url = 'https://tululu.org/b'
 
 for i in range(1, 10):
@@ -55,20 +99,9 @@ for i in range(1, 10):
 
     soup = BeautifulSoup(response.text, 'lxml')
 
-    book_name = soup.find('h1').text.split('::')[0]
-    book_url = soup.find('a', text='скачать txt')
-    if book_url:
-        #download_txt(f'{url[:-2]}{book_url["href"]}', book_name)
-        #book_image_url = soup.find('div', class_='bookimage').find('a').find('img')
-        #if book_image_url:
-        #    download_img(urljoin(url, book_image_url["src"]))
+    print(str(i))
+    parse_book_page(soup)
 
-        #book_comments = soup.find_all('div', class_='texts')
-        #print(book_name)
-        #for comment in book_comments:
-        #    print(f'-- {comment.find("span", class_="black").text}')
 
-        print(book_name)
-        book_genres = soup.find('span', class_='d_book').find_all('a')
-        for genre in book_genres:
-            print(genre.text, end=',')
+
+
