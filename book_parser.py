2e3437d187efcbd57cc72e27a08790d12636dd0e
statik2002 <statik2002@mail.ru>
2022-09-27 18:53:24 +0800

    download books from all pages beta

--- book_parser.py
+++ book_parser.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import time
 import sys
 import requests
@@ -92,6 +93,8 @@ def parse_book_page(url, page_soup):
         ).find('a').find('img')
 
     book_txt_url = urljoin(url, book_url['href'])
+    if not book_txt_url:
+        return
 
     book_comments_raw = page_soup.find_all('div', class_='texts')
     book_comments = [comment.find("span", class_="black").text for comment in book_comments_raw]
@@ -110,36 +113,63 @@ def parse_book_page(url, page_soup):
     return book
 
 
+def get_books_links(url, num_pages=2):
+
+    book_links = []
+    for page in range(num_pages):
+        response = requests.get(urljoin(url, str(page)))
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        books = soup.find_all('table', class_='d_book')
+
+        for book in books:
+            book_url = book.find('div', class_='bookimage').find('a')
+            book_links.append(urljoin(url, book_url['href']))
+
+    return book_links
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Скрипт для скачивание книг с сайта https://tululu.org/',
     )
-    parser.add_argument('start_id', help='с какой книги (число)', type=int)
-    parser.add_argument('end_id', help='по какую книгу (число)', type=int)
+    #parser.add_argument('start_id', help='с какой книги (число)', type=int)
+    #parser.add_argument('end_id', help='по какую книгу (число)', type=int)
     args = parser.parse_args()
 
     url = 'https://tululu.org/'
+    sci_fi_url = 'https://tululu.org/l55/'
 
     main_folder = 'books'
 
+    book_catalog = []
+
     Path(main_folder).mkdir(exist_ok=True)
 
-    for book_id in range(args.start_id, args.end_id):
+    book_links = get_books_links(sci_fi_url)
+
+    for book_link in book_links:
 
         total_connection_try, current_connection_try = 5, 0
 
         while True and current_connection_try < total_connection_try:
 
             try:
-                response = requests.get(urljoin(url, f'b{str(book_id)}/'))
+                response = requests.get(book_link)
                 response.raise_for_status()
 
                 check_for_redirect(response)
                 soup = BeautifulSoup(response.text, 'lxml')
 
                 book = parse_book_page(response.url, soup)
+                if not book:
+                    break
 
                 download_book(book, main_folder)
+
+                book_catalog.append(book)
 
                 break
 
@@ -160,6 +190,9 @@ def main():
             except requests.exceptions.TooManyRedirects:
                 print('Слишком много редиректов')
                 break
+
+    with open('books.json', 'w') as books_file:
+        books_file.write(json.dumps(book_catalog, ensure_ascii=False))
 
 
 if __name__ == '__main__':
